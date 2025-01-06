773fd032b982414fc09d8dbb980ad18a2e46bee0
prasanna kumar J <prasannakumarjagabathina@gmail.com>
2025-01-06 15:16:03 +0530

    Update 04-variables.sh

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1 # No space
+PERSON2=2$
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hello $PERSON1"
+echo "$PERSON1:: how are you $PERSON2"
+echo "$PERSON2:: I am fine $PERSON1"
